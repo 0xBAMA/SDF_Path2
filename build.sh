5eeef10b389950ddb8e65fcc2a60b61b4dfa7fcb
0xBAMA <jon.baker1222@gmail.com>
2021-11-25 20:07:03 -0600

    fixed fastnoise noisetool build stuff

--- build.sh
+++ build.sh
@@ -7,7 +7,16 @@ make exe
 cp ./Release/bin/exe ..
 cd ..
 
+if [ "$1" == "noiseTool" ]
+then
+  cd build/resources/FastNoise2/NoiseTool/
+  make
+  cd ../../..
+  cp ./Release/bin/NoiseTool ..
+  cd ..
+fi
+
 if [ "$1" == "clean" ]
 then
-   rm -r ./build
+  rm -r ./build
 fi
